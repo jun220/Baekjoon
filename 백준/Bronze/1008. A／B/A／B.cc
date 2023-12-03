903fe82632df93cc47b766b557f89adf8689b006
jun220 <152267292+jun220@users.noreply.github.com>
2023-12-03 12:15:12 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+using namespace std;
+
+int main(void) {
+    double A, B;
+    cin >> A >> B;
+    cout.precision(10);
+    cout << A / B;
+    return 0;
+}
