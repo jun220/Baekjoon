e245131ffdb223e6db5bba767134daf7eb46d8fe
jun220 <152267292+jun220@users.noreply.github.com>
2023-12-03 12:11:21 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+using namespace std;
+
+int main(void) {
+    long A, B, C;
+    cin >> A >> B >> C;
+    cout << A + B + C;
+    return 0;
+}
