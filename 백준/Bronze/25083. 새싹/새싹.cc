89f796eabe826e69246fc81722824ba288498540
jun220 <152267292+jun220@users.noreply.github.com>
2023-12-03 12:05:26 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+using namespace std;
+
+int main(void) {
+    cout << "         ,r'\"7\n";
+    cout << "r`-_   ,'  ,/\n";
+    cout << " \\. \". L_r\'\n";
+    cout << "   `~\\/\n";
+    cout << "      |\n";
+    cout << "      |";
+    return 0;
+}
