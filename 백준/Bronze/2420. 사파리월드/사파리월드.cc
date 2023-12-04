6794f8b871e8b3ee6c64c8a61151e2cc9535e79e
jun220 <152267292+jun220@users.noreply.github.com>
2023-12-05 02:51:33 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(void) {
+    long N, M;
+    cin >> N >> M;
+    cout << abs(N - M);
+
+    return 0;
+}
