db835176f69ea68d46033fe1840504b60e150d32
jun220 <152267292+jun220@users.noreply.github.com>
2023-12-05 03:15:09 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(void) {
+    int N;
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
