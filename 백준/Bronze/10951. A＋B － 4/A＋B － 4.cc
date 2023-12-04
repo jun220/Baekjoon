b3e5b2fcad8df662165fd4427dd3cce676035f6b
jun220 <152267292+jun220@users.noreply.github.com>
2023-12-05 03:51:21 +0900

    [Bronze V] Title: A+B - 4, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(void) {
+    int A, B;
+    while(cin >> A >> B) {
+        cout << A + B << '\n';
+    }
+
+    return 0;
+}
