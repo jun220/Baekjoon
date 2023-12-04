4b09d2c43d1a738bb00e4df6f2b2e5276513e92a
jun220 <152267292+jun220@users.noreply.github.com>
2023-12-05 02:36:48 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+using namespace std;
+
+int main(void) {
+    int A, B;
+    cin >> A >> B;
+    if (A > B) cout << '>';
+    else if (A < B) cout << '<';
+    else cout << "==";
+    return 0;
+}
