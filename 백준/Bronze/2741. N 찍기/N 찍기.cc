7f5c2c673598b8c4e17c4625269f93bbf111f34d
jun220 <152267292+jun220@users.noreply.github.com>
2023-12-05 02:53:51 +0900

    [Bronze V] Title: N 찍기, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(void) {
+    int N;
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        cout << i << '\n';
+    }
+
+    return 0;
+}
