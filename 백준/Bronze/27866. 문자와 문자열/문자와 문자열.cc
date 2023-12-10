72c8c17a17f3ff3a3f0077d971b5b93ba93bd929
jun220 <152267292+jun220@users.noreply.github.com>
2023-12-10 17:27:03 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+#include <array>
+#include <vector>
+#include <algorithm>
+//#include "Rikkabrary.h"
+
+using namespace std;
+
+
+
+int main(void) {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    
+    string S;
+    int i;
+
+    cin >> S >> i;
+    cout << S[i-1];
+
+    
+
+    return 0;
+}
