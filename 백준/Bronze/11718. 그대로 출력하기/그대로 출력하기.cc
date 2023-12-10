e841d231c0f6727e5c9a75e190fdf56656ecdfa9
jun220 <152267292+jun220@users.noreply.github.com>
2023-12-10 17:35:22 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+#include <array>
+#include <vector>
+#include <algorithm>
+#include <string>
+//#include "Rikkabrary.cpp"
+
+using namespace std;
+
+
+
+int main(void) {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    string word;
+    
+    while (getline(cin, word)) {
+        cout << word << '\n';
+    }
+
+    
+
+    return 0;
+}
