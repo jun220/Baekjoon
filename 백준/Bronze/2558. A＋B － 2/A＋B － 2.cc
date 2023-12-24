d58173c07465981919243acd13ac3d9a981ae99f
Jun220 <152267292+jun220@users.noreply.github.com>
2023-12-24 23:39:19 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <forward_list>
+#include <string>
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false); // 입출력 속도 향상을 위해
+    cin.tie(NULL);
+
+    int A, B;
+    cin >> A >> B;
+    cout << A+B;
+
+
+    return 0;
+}
