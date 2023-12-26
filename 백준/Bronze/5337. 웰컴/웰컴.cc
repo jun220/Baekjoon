c4d78f7feb461a9da816f4de2e3ac1ceb8906f36
Jun220 <152267292+jun220@users.noreply.github.com>
2023-12-26 23:55:27 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <forward_list>
+#include <string>
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false); // 입출력 속도 향상을 위해
+    cin.tie(NULL);
+
+    int A, B;
+    cout << ".  .   ." << endl;
+    cout << "|  | _ | _. _ ._ _  _" << endl;
+    cout << "|/\\|(/.|(_.(_)[ | )(/." << endl;
+
+
+
+    return 0;
+}
