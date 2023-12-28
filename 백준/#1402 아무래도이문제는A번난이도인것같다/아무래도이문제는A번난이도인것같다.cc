0dbdb03190e9e041730c53eac737ac01644f992f
Jun220 <152267292+jun220@users.noreply.github.com>
2023-12-29 04:49:13 +0900

    [Silver V 1402] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2020 KB -NKLCBHub

---
+++ 백준/#1402 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <forward_list>
+#include <string>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); // 입출력 속도 향상을 위해
+    cin.tie(NULL);
+
+    int N;
+    long long A, B;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> A >> B;
+        cout << "yes" << '\n';
+    }
+
+
+    return 0;
+}
