457a2734d09eb837fd5a1db045b2b300fb52209c
Jun220 <152267292+jun220@users.noreply.github.com>
2023-12-29 04:58:46 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <forward_list>
+#include <string>
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false); // 입출력 속도 향상을 위해
+    cin.tie(NULL);
+
+    string s;
+    cin >> s;
+    cout << s << "??!";
+
+
+
+    
+
+
+    return 0;
+}
