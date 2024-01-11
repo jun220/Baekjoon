13e81473b37930882337bf7f381c83bd841fedd8
Jun220 <152267292+jun220@users.noreply.github.com>
2024-01-11 23:59:22 +0900

    [Bronze IV] Title: 기찍 N, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    
+    int N;
+    cin >> N;
+    for (int i = N; i > 0; i--) {
+        cout << i << '\n';
+    }
+
+    return 0;
+}
