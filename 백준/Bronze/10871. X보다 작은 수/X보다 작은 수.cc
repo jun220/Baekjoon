0ca57117baa793df2810b3ed4a15eb288179ec33
Jun220 <152267292+jun220@users.noreply.github.com>
2024-03-10 17:09:53 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -1,26 +1,21 @@
 #include <iostream>
-#include <iomanip>
-#include <cstdlib>
-#include <ctime>
-#include <array>
 #include <vector>
 using namespace std;
 
-int main(void) {
-    cin.tie(NULL);
-    ios_base::sync_with_stdio(false);
 
-    int N, X, temp;
-    vector<int> v;
-    cin >> N >> X;
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     
+    int N, X, temp;
+
+    cin >> N >> X;
+
     for (int i = 0; i < N; i++) {
         cin >> temp;
-        v.push_back(temp);
+        if (temp < X) cout << temp << ' ';
     }
 
-    for (int n : v) {
-        if (n < X) cout << n << " ";
-    }
     return 0;
 }
