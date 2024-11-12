969b3a09925f1e05c52880121ddc79c624a494ad
Jun220 <152267292+jun220@users.noreply.github.com>
2024-11-12 15:57:31 +0900

    [Gold V] Title: 토마토, Time: 100 ms, Memory: 7892 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.cc
+++ 백준/Gold/7569. 토마토/토마토.cc
@@ -7,39 +7,45 @@ struct Tomato {
     int x;
     int y;
     int z;
+    //int count;
 };
 
 int dx[] = {1, -1, 0, 0, 0, 0};
-int dy[] = {0, 0, 1, -1, 0, 0};
-int dz[] = {0, 0, 0, 0, 1, -1};
+int dy[] = { 0, 0, 1, -1, 0, 0 };
+int dz[] = { 0, 0, 0, 0, 1, -1 };
 
-int main() {
+
+int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
     int M, N, H;
     cin >> M >> N >> H;
 
-    vector<vector<vector<int>>> v(H, vector<vector<int>>(N, vector<int>(M, 0)));
+    vector<vector<vector<int>>> 
+        v(H, vector<vector<int>>(N, vector<int>(M, 0)));
 
     bool isAllRiped = true;
-    queue<Tomato> Q;
 
+    //vector<vector<vector<bool>>>
+    //    visited(H, vector<vector<bool>>(N, vector<bool>(M, 0)));
+
+    int temp;
+    queue<Tomato> Q;
+    //cout << "이제 토마토를 입력받겠습니다\n";
     for (int z = 0; z < H; z++) {
         for (int y = 0; y < N; y++) {
             for (int x = 0; x < M; x++) {
-                cin >> v[z][y][x];
-                if (v[z][y][x] == 1) {
-                    Q.push({x, y, z});
-                }
-                if (v[z][y][x] == 0) {
-                    isAllRiped = false;  // 익지 않은 토마토가 하나라도 있으면 false
-                }
+                cin >> temp;
+                if (temp == 1) Q.push({ x,y,z });
+                if (temp == 0) isAllRiped = false;
+                //cout << "(" << z << ',' << y << ',' << x << ")\n";
+                v[z][y][x] = temp;
             }
         }
     }
 
-    if (isAllRiped) {  // 처음부터 모두 익어있는 경우
+    if (isAllRiped) {
         cout << 0;
         return 0;
     }
@@ -48,32 +54,49 @@ int main() {
         Tomato t = Q.front();
         int dist = v[t.z][t.y][t.x];
         Q.pop();
+        /*cout << "tomato : " << t->x
+            << ',' << t->y
+            << ',' << t->z << '\n';*/
 
         for (int i = 0; i < 6; i++) {
             int nx = t.x + dx[i];
             int ny = t.y + dy[i];
             int nz = t.z + dz[i];
 
-            if (nx < 0 || nx >= M || ny < 0 || ny >= N || nz < 0 || nz >= H) continue;
-            if (v[nz][ny][nx] != 0) continue;  // 익지 않은 토마토(0)만 탐색
+            // x,y,z의 범위 체크
+            if (nx < 0 || nx >= M) continue;
+            if (ny < 0 || ny >= N) continue;
+            if (nz < 0 || nz >= H) continue;
 
+            // 이미 익은 토마토이거나 비어 있는 칸이면 건너뛰기
+            if (v[nz][ny][nx] != 0) continue;
+            
             v[nz][ny][nx] = dist + 1;
+
             Q.push({nx, ny, nz});
         }
     }
 
-    int maxDays = 0;
+    //cout << "이제 출력합니다";
+
+    int max = 0;
+    bool isClear = true;
+
     for (int z = 0; z < H; z++) {
         for (int y = 0; y < N; y++) {
             for (int x = 0; x < M; x++) {
-                if (v[z][y][x] == 0) {  // 익지 않은 토마토가 남아있다면 -1 출력
+                int dist = v[z][y][x];
+                if (dist == 0) {
                     cout << -1;
                     return 0;
                 }
-                maxDays = max(maxDays, v[z][y][x]);
+                if(dist > max) max = dist;
+                //cout << dist << ' ';
             }
+            //cout << '\n';
         }
     }
 
-    cout << maxDays - 1;  // 익는 일수를 계산하기 위해 -1
+    cout << max-1;
+
 }
