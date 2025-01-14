2cbb39e23fc3de4bd029c94113f4edff978db6ad
Jun220 <152267292+jun220@users.noreply.github.com>
2025-01-15 07:30:45 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <queue>
+#include <vector>
+#include <stack>
+#include <vector>
+#include <cmath>
 using namespace std;
-
-vector<vector<int>> v;
-vector<vector<bool>> visited;
 
 #define X first
 #define Y second
@@ -11,93 +12,62 @@ vector<vector<bool>> visited;
 int dx[4] = { 1, 0, -1, 0 };
 int dy[4] = { 0, 1, 0, -1 };
 
-
-
-
-
-
-
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int N, M;
-    cin >> N >> M;
-    //vector<vector<int>> maze(N, vector<int>(M));
-    vector<vector<int>> maze;
 
-    string maze_string;
+    int M, N;
+    cin >> M >> N;
 
-    for (int x = 0; x < N; x++) {
-        vector<int> temp;
-        cin >> maze_string;
-        for (char c : maze_string) {
-            //cout << "c: " << c << '\n';
-            if (c == '1') temp.push_back(1);
-            else temp.push_back(0);
-        }
-        //cout << "temp: ";
-        //for (int n : temp) cout << n;
-        //cout << '\n';
-        maze.push_back(temp);
-        //maze[N] = temp;
+    vector<string> board(M, "");
+    vector<vector<int>> distance(M, vector<int>(N, 0));
+    
+    for (int x = 0; x < M; x++) {
+        cin >> board[x];
     }
 
-    //for (int x = 0; x < N; x++) {
-    //    for (int y = 0; y < M; y++) {
-    //        cout << maze[x][y] << ' ';
-    //    }
-    //    cout << '\n';
-    //}
-
-    // 목표위치 : maze[x-1][y-1]
-
-    vector<vector<int>> distance(N, vector<int>(M, -1));
+    // BFS
     queue<pair<int, int>> Q;
-
-    distance[0][0] = 0;
-    Q.push({ 0, 0 });
     
-    int dist = 0;
+    Q.push({ 0,0 });
+    distance[0][0] = 1;
+
+    int distance_to_end = 1;
 
     while (!Q.empty()) {
-        pair<int, int> node = Q.front();
-        Q.pop();
-        //cout << "node : (" << node.X << ',' << node.Y
-        //    << "), dist : " << dist << '\n';
-        dist = distance[node.X][node.Y];
+        pair<int, int> cur = Q.front(); Q.pop();
+        int dist = distance[cur.X][cur.Y] + 1;
+        
+        for (int dir = 0; dir < 4; dir++) {
+            int nx = cur.X + dx[dir];
+            int ny = cur.Y + dy[dir];
+
+            if (nx < 0 || nx >= M || ny < 0 || ny >= N) continue;
+            if (distance[nx][ny] != 0) continue; // 거리가 0일 경우만 아직 확인하지 않은 노드. 이외의 값일 경우 이미 체크했다는 뜻
+            if (board[nx][ny] != '1') continue; // 1인 칸이 아니면 검사할 필요 자체가 없음
 
 
-        for (int i = 0; i < 4; i++) {
-            int nx = node.X + dx[i];
-            int ny = node.Y + dy[i];
-
-            if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
-                //cout << "범위를 벗어났습니다\n";
-                continue;
-            }
-            if (distance[nx][ny] != -1) {
-                //cout << "여긴 이미 거리 처리가 되어있네요\n";
-                continue;
-            }
-            if (maze[nx][ny] == 0) {
-                //cout << "거긴 벽입니다\n";
-                continue;
-            }
-
-            distance[nx][ny] = dist + 1;
             Q.push({ nx, ny });
+            distance[nx][ny] = dist;
+
+            if (nx == M - 1 && ny == N - 1) {
+                //cout << "미로의 끝에 도착!\n";
+                distance_to_end = dist;
+            }
         }
     }
-
-    //for (int x = 0; x < N; x++) {
-    //    for (int y = 0; y < M; y++) {
+    
+    //for (int x = 0; x < M; x++) {
+    //    for (int y = 0; y < N; y++) {
     //        cout << distance[x][y] << ' ';
     //    }
     //    cout << '\n';
     //}
+    cout << distance_to_end;
+    
+    
 
-    cout << distance[N - 1][M - 1] + 1;
 
 
 }
