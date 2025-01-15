3d7edb93faac85abe0c1eab4d2b671258a9b3e1f
Jun220 <152267292+jun220@users.noreply.github.com>
2025-01-16 02:01:10 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -1,32 +1,25 @@
 #include <iostream>
-#include <queue>
-#include <vector>
 #include <stack>
-#include <vector>
 using namespace std;
 
-#define ll long long
 
-ll func2(ll a, ll b, ll m) {
-    ll val = 1;
+long long func2(int a, int b, int m) {
     if (b == 0) return 1;
+
     if (b % 2 == 0) {
-        ll result = func2(a, b / 2, m) % m;
-        return result * result % m;
+        long long temp = func2(a, b / 2, m);
+        return temp * temp % m;
     }
     else {
         return a * func2(a, b - 1, m) % m;
     }
 }
 
-
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    ll a, b, c;
+    int a, b, c;
     cin >> a >> b >> c;
-
     cout << func2(a, b, c);
-
 }
