b406f4853a8c43e9f966b1c5a68c430cac621208
Jun220 <152267292+jun220@users.noreply.github.com>
2025-02-22 19:57:42 +0900

    [Silver V] Title: 배열 합치기, Time: 424 ms, Memory: 17644 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.cc
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.cc
@@ -3,56 +3,52 @@
 #include <queue>
 using namespace std;
 
+int A[1000005];
+int B[1000005];
+int C[2000005];
 
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
+
+
     int N, M;
     cin >> N >> M;
-    list<int> A;
-    list<int> B;
-    list<int> C;
 
-    int temp;
+    for (int i = 0; i < N; i++) cin >> A[i];
+    for (int i = 0; i < M; i++) cin >> B[i];
 
-    for (int i = 0; i < N; i++) {
-        cin >> temp;
-        A.push_back(temp);
-    }
-    for (int i = 0; i < M; i++) {
-        cin >> temp;
-        B.push_back(temp);
-    }
+    int aIdx = 0;
+    int bIdx = 0;
+    
+    for (int i = 0; i < M + N; i++) {
+        if (aIdx == N) {
+            C[i] = B[bIdx];
+            bIdx++;
+            continue;
+        }
+        if (bIdx == M) {
+            C[i] = A[aIdx];
+            aIdx++;
+            continue;
+        }
+        
+        int aValue = A[aIdx];
+        int bValue = B[bIdx];
 
-
-    auto curA = A.begin();
-    auto curB = B.begin();
-
-    while (curA != A.end() && curB != B.end()) {
-        if (*curA < *curB) {
-            C.push_back(*curA);
-            curA++;
+        if (aValue <= bValue) {
+            C[i] = A[aIdx];
+            aIdx++;
         }
         else {
-            C.push_back(*curB);
-            curB++;
+            C[i] = B[bIdx];
+            bIdx++;
         }
     }
 
-    while (curA != A.end()) {
-        C.push_back(*curA);
-        curA++;
+    for (int i = 0; i < M + N; i++) {
+        cout << C[i] << ' ';
     }
-
-    while (curB != B.end()) {
-        C.push_back(*curB);
-        curB++;
-    }
-
-    for (int n : C) {
-        cout << n << ' ';
-    }
-
 
 }
